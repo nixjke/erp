baaa97aa429e5a9467bc358100d9562b47286590
SosiskaPish <aziz.10.03@mail.ru>
2022-04-14 13:26:29 +0300

    init normalize.css

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import './App.css'
 
 function App() {
-  return <div className="App">test</div>
+  return <div className="App">test
+  <a href="asd">asd</a>
+  <p>asdasd</p></div>
 }
 
 export default App
