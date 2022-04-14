195684a9bb5e1475cfedf8df36d6ae60f36c5679
SosiskaPish <aziz.10.03@mail.ru>
2022-04-14 17:30:51 +0300

    add AuthPlatform

--- src/components/AuthPlatform/AuthPlatform.tsx
+++ src/components/AuthPlatform/AuthPlatform.tsx
@@ -1,6 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import styles from './AuthPlatform.module.scss'
 
 export default function AuthPlatform() {
-  return <div className={styles.authPlatform}>Auth Platform</div>
+  return (
+    <div className={styles.authPlatform}>
+      <div className={styles.form}>
+        <Link to="/" className={styles.close}>
+          X
+        </Link>
+        <form className={styles.authForm}>
+          <h1>Войти в аккаунт</h1>
+          <div className={styles.inputs}>
+            <input type="text" placeholder="Название платформы" />
+            <input type="text" value=".mysmartoffice.pro" readOnly />
+          </div>
+          <button className={styles.button}>Войти</button>
+        </form>
+      </div>
+    </div>
+  )
 }
