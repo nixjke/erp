392d30770f07d3f7f9c64c1346e873f96927b573
SosiskaPish <aziz.10.03@mail.ru>
2022-04-17 14:34:40 +0300

    clear AuthForm

--- src/components/AuthForm/AuthForm.tsx
+++ src/components/AuthForm/AuthForm.tsx
@@ -8,41 +8,29 @@ export default function AuthForm() {
   const dispath = useDispatch()
   const signin = useAppSelector(store => store.signin)
 
-  const { data } = signin.item
-
-  const [blockTitle, setBlockTitle] = React.useState(data.blocks[0].BlockTitle)
-  const [emailField, setEmailField] = React.useState(data.blocks[0].Fields[0])
-  const [passwordField, setPasswordField] = React.useState(data.blocks[0].Fields[1])
-  const [rememberMeField, setRememberMeField] = React.useState(data.blocks[0].Fields[2])
-  const [haveAccountText, setHaveAccountText] = React.useState(data.blocks[0].Texts[0])
-  const [rightsReserved, setRightsReserved] = React.useState(data.blocks[0].Texts[1])
-  const [forgetPassword, setForgetPassword] = React.useState(data.Links[0])
-  const [register, setRegister] = React.useState(data.Links[1])
-  const [signInButton, setSignInButton] = React.useState(data.Buttons[0])
-
   React.useEffect(() => {
     dispath(fetchContainer())
   }, [])
 
   return (
     <div className={styles.authForm}>
-      <form className={styles.form}>
+      {/* <form className={styles.form}>
         <h1>{blockTitle}</h1>
         <div className={styles.inputs}>
-          <input className={styles.email} type="text" placeholder="Email" />
-          <input className={styles.password} type="text" placeholder="Пароль" />
+          <input className={styles.email} type="text" placeholder={emailField.title} />
+          <input className={styles.password} type="text" placeholder={passwordField.title} />
         </div>
-        <a href="">Забыли пароль?</a>
+        <a href="">{`${forgetPassword.Title}?`}</a>
         <label className={styles.label}>
-          <input type="checkbox" />
-          <span> Запомнить меня</span>
+          <input type={rememberMeField.type} />
+          <span>{rememberMeField.title}</span>
         </label>
-        <button className={styles.button}>Войти</button>
+        <button className={styles.button}>{signInButton.title}</button>
         <div className={styles.register}>
-          <p>Еще нет аккаунта?</p>
-          <a href="">Зарегистируйтесь бесплатно</a>
+          <p>{haveAccountText.title}</p>
+          <a href="">{register.Title}</a>
         </div>
-      </form>
+      </form> */}
     </div>
   )
 }
