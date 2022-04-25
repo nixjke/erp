b2e0796368d82247c9f460d3556743e1a375fcd8
nixjke <aziz.10.255@gmail.com>
2022-04-25 16:57:25 +0300

    clear AuthForm

--- src/components/AuthForm/AuthForm.tsx
+++ src/components/AuthForm/AuthForm.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { useAppSelector } from '../../store/redux-hooks'
 import { fetchContainer } from '../../store/slices/siginSlice/signinSlice'
+import ShadowBox from '../ShadowBox/ShadowBox'
+import Text from '../Text/Text'
 import s from './AuthForm.module.scss'
 
 export default function AuthForm() {
@@ -42,55 +44,7 @@ export default function AuthForm() {
 
   return (
     <div className={s.authForm}>
-      <form className={s.form}>
-        <h1>{signin.data.blocks[0].BlockTitle}</h1>
-        {allBlocks.map(block => {
-          if (block.type === 'Email') {
-            return (
-              <div>
-                <input
-                  onBlur={e => blurHandle(e)}
-                  name={block.type}
-                  key={block.title + block.id}
-                  className={s.email}
-                  type="text"
-                  placeholder="Email"
-                />{' '}
-                {(emailDirty && emailError) && <div className={s.error}>{emailError}</div>}
-              </div>
-            )
-          }
-          if (block.type === 'password')
-            return <input key={block.title + block.id} className={s.password} type="text" placeholder="Пароль" />
-          if (block.title === 'Забыли пароль')
-            return (
-              <a key={block.title + block.id} href="">
-                {signin.data.Links[0].title}
-              </a>
-            )
-          if (block.type === 'checkbox')
-            return (
-              <label key={block.title + block.id} className={s.label}>
-                <input type="checkbox" />
-                <span>{block.title}</span>
-              </label>
-            )
-          if (block.title === 'Войти')
-            return (
-              <button key={block.title + block.id} className={s.button}>
-                {signin.data.Buttons[0].title}
-              </button>
-            )
-          if (block.title === 'Еще нет аккаунта?')
-            return <p key={block.title + block.id}>{signin.data.blocks[0].Texts[0].title}</p>
-          if (block.title === 'Зарегистрируйтесь бесплатно')
-            return (
-              <a key={block.title + block.id} href="">
-                {signin.data.Links[1].title}
-              </a>
-            )
-        })}
-      </form>
+      <ShadowBox></ShadowBox>
     </div>
   )
 }
