0b59f6c9ac6342f478c84d0a601231faf9b35e26
nixjke <aziz.10.255@gmail.com>
2022-05-03 11:18:58 +0300

    fix render Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import styles from './Header.module.scss'
 import logo from '../../assets/image/LogoMySmartOffice.svg'
+import { useAppSelector } from '../../store/redux-hooks'
 
 export default function Header() {
+  const signin = useAppSelector(store => store.signin)
+  console.log(signin.item.header)
+
+  if(!signin.isLoading) return <header><h1>Loading...</h1></header>
+
   return (
     <header className={styles.header}>
       <div className={styles.wrapper}>
-        <img src={logo} alt="logo" />
-        <div>|  CRM CompanyName</div>
+        <img src={logo} alt={signin.item.header.Image[0].title} />
+        <div>| {signin.item.header.Texts[0].title}</div>
       </div>
     </header>
   )
