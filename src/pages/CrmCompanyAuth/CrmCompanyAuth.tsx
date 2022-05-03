be823a6eed6f9927428a53fffbc2906b4d19bcd2
nixjke <aziz.10.255@gmail.com>
2022-05-03 18:31:30 +0300

    add background image

--- src/pages/CrmCompanyAuth/CrmCompanyAuth.tsx
+++ src/pages/CrmCompanyAuth/CrmCompanyAuth.tsx
@@ -4,9 +4,16 @@ import Footer from '../../components/Footer/Footer'
 import Header from '../../components/Header/Header'
 
 export default function CrmCompanyAuth() {
-
   return (
-    <div>
+    <div
+      style={{
+        backgroundImage:
+          'url(https://sun9-61.userapi.com/s/v1/if2/ci9fT513HSYjUGJcq2B4DtMPt1h26bnKXQyhT_bFhAxqZal0jvzUK5mZSZDqkJ5TrXHi-pCorhZnkjiUNnctorMq.jpg?size=1920x1080&quality=96&type=album)',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+      }}
+    >
       <Header />
 
       <Outlet />
